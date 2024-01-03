ba494dafe48db2c56e6285c8eb84e8602aa11a43
Omakmusae <pak1208926@gmail.com>
2024-01-03 22:36:51 +0900

    타입스크립트 관련 모듈 추가

--- example.ts
+++ example.ts
@@ -1,11 +1,14 @@
 // import AWS from 'aws-sdk';//비추천하는 aws sdk 사용 방법
 // import S3 from 'aws-sdk/clients/s3' //필요한 S3 모듈만 불러오는 aws sdk 사용 방법 - 추천
 // import dotenv from 'dotenv'
+
 // dotenv.config();
+
 // // AWS.config.update({
 // //     accessKeyId: process.env.S3_ACCESS_KEY_ID,
 // //     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
 // //     region: 'ap-northeast-2',
 // //  });
+
 // console.log(process.env.S3_ACCESS_KEY_ID)
-console.log("sdfsfd");
+console.log("sdfsfd")
